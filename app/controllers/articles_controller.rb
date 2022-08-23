e7a51cb5df38b93caf10bc7b79a03ebdf8ebea21
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-08-23 19:12:38 +0300

    Implement articles#edit, articles#update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,13 +15,26 @@ class ArticlesController < ApplicationController
 
     @article = Article.new(article_params)
     
-    if @article.valid?
-      @article.save
+    if @article.save
       redirect_to @article
     else
       render action: 'new'
     end 
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render action: 'edit'
+    end 
+  end  
 
   private
 
