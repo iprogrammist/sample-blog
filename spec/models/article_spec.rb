41d66a429cd4c1a6a3575b194daaa451c8fc74c8
iprogrammist <110420915+iprogrammist@users.noreply.github.com>
2022-09-06 07:12:08 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe Article, type: :model do
   describe "associations" do  
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаём объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
+
 end  
